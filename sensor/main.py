b972edfc4fcd65c28c90042cf905f44af8d86cc0
MaxJ02 <ullman4444@gmail.com>
2023-09-21 11:03:12 +0200

    second commit, add config.json

--- sensor/main.py
+++ sensor/main.py
@@ -1,1 +1,12 @@
-#test commit
+#with open ('config.json') as pin_interval:
+#    file_contents = pin_interval.read()
+#
+#    print (file_contents)
+
+import time
+import machine
+from machine import Pin
+from onewire import OneWire
+from ds18x20 import DS18X20
+
+
