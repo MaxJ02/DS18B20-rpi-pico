f0ece092ec6316e15aa5c772c08c7021568121f2
MaxJ02 <ullman4444@gmail.com>
2023-09-28 10:45:19 +0200

    cleaned up the comments. Not formated yet.

--- sensor/main.py
+++ sensor/main.py
@@ -6,11 +6,11 @@ import ujson
 with open('config.json', 'r') as f:
     config = ujson.load(f)
 
-dat_pin_number = config.get('pin', 16)
-sleep_time = config.get('interval', 900)
+dat_pin_number = config.get('pin', 16)  # reads the pin number from the config file, defaults to 16
+sleep_time = config.get('interval', 9) # reads the interval from the config file, defaults to 9 seconds
 
-# the device is connected to GPIO16
-dat = machine.Pin(dat_pin_number) #placeholder to test the code, will be loaded from the json file later
+# The device is connected to GPIO16 
+dat = machine.Pin(dat_pin_number)
 
 # creates the onewire object
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
@@ -20,18 +20,17 @@ ds = ds18x20.DS18X20(onewire.OneWire(dat))
 roms = ds.scan()
 print('found devices:', roms)
 
-
+# Reads the unique id of the device and converts it to a hex string
 id = ""
 for b in machine.unique_id():
   id += "{:02x}".format(b)
 
-sensor_id = hex(int.from_bytes(b'(\xabG8N \x01\x0c', 'little'))[2:]
 
-
-# loop 10 times and print all temperatures
+# loop to read the temperature from the sensor and print it to the console infinitely
 while True:
     ds.convert_temp()
-    time.sleep_ms(sleep_time) #placeholder, to be changed to read from config file later
+    time.sleep(sleep_time)
     for rom in roms:
-        print(id, sensor_id, ds.read_temp(rom), end=' ') # Needs adjustments
+        sensor_id = hex(int.from_bytes(rom, 'little'))[2:]
+        print(id, sensor_id, ds.read_temp(rom), end=' ')
     print()
