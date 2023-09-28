ef6dd44e3c7e4169d7598aae532d324e0e92be9f
MaxJ02 <ullman4444@gmail.com>
2023-09-28 08:43:03 +0200

    restructured the code

--- sensor/main.py
+++ sensor/main.py
@@ -1,12 +1,25 @@
-#with open ('config.json') as pin_interval:
-#    file_contents = pin_interval.read()
-#
-#    print (file_contents)
-
 import time
 import machine
-from machine import Pin
-from onewire import OneWire
-from ds18x20 import DS18X20
+import onewire, ds18x20
+
+# the device is on GPIO16
+dat = machine.Pin(16) #placeholder to test the code, will be loaded from the json file later
+
+# creates the onewire object
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
 
 
+# scan for devices on the bus
+roms = ds.scan()
+print('found devices:', roms)
+
+sensor_id = hex(int.from_bytes(b'(\xabG8N \x01\x0c', 'little'))
+
+
+# loop 10 times and print all temperatures
+for i in range(10):
+    ds.convert_temp()
+    time.sleep_ms(750) #placeholder, to be changed to read from config file later
+    for rom in roms:
+        print(sensor_id, ds.read_temp(rom * 1000), end=' ') # Needs adjustments
+    print()
