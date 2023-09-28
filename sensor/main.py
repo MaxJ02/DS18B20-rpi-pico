4be5303830111db5d9f43a97882a871dc42cc3e3
MaxJ02 <ullman4444@gmail.com>
2023-09-28 10:17:17 +0200

    added json functionality

--- sensor/main.py
+++ sensor/main.py
@@ -1,9 +1,16 @@
 import time
 import machine
 import onewire, ds18x20
+import ujson
+
+with open('config.json', 'r') as f:
+    config = ujson.load(f)
+
+dat_pin_number = config.get('pin', 16)
+sleep_time = config.get('interval', 900)
 
 # the device is connected to GPIO16
-dat = machine.Pin(16) #placeholder to test the code, will be loaded from the json file later
+dat = machine.Pin(dat_pin_number) #placeholder to test the code, will be loaded from the json file later
 
 # creates the onewire object
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
@@ -22,9 +29,9 @@ sensor_id = hex(int.from_bytes(b'(\xabG8N \x01\x0c', 'little'))[2:]
 
 
 # loop 10 times and print all temperatures
-for i in range(10):
+while True:
     ds.convert_temp()
-    time.sleep_ms(750) #placeholder, to be changed to read from config file later
+    time.sleep_ms(sleep_time) #placeholder, to be changed to read from config file later
     for rom in roms:
         print(id, sensor_id, ds.read_temp(rom), end=' ') # Needs adjustments
     print()
